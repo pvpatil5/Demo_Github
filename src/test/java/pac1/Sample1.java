4f53c0e3c9c1a79676c155dca932bc3cd6965909
AMAR-G <AMAR-G@Amar>
2020-07-09 09:25:37 +0530

    First Commit

---
+++ src/test/java/pac1/Sample1.java
@@ -0,0 +1,18 @@
+package pac1;
+
+public class Sample1 {
+
+	public static void main(String[] args)
+	{
+		String s1= "Selenium";
+		
+		char c[] = s1.toCharArray();
+
+		for (int i = c.length-1; i >=0; i--) 
+		{
+			System.out.print(c[i]);
+		}
+
+	}
+
+}
